eb3dc532f95d85c6652f3cac06cfc6790def5d24
higor-rfp <higorp@gmail.com>
2026-06-08 23:24:05 +0000

    lab7

--- lab07-microfrontends/static/carrinho.js
+++ lab07-microfrontends/static/carrinho.js
@@ -2,7 +2,7 @@ const cart = { items: [], total: 0 };
 function updateCartUI() {
     const list = document.getElementById('cart-list');
     const totalEl = document.getElementById('cart-total');
-    list.innerHTML = cart.items.length === 0 ? 'Vazio' : '';
+     list.innerHTML = cart.items.length === 0 ? 'Vazio' : '';
     cart.items.forEach(item => {
         const div = document.createElement('div');
         div.innerText = `${item.nome} - R$ ${item.preco}`;
@@ -11,3 +11,13 @@ function updateCartUI() {
     totalEl.innerText = cart.total.toFixed(2);
 }
 updateCartUI();
+
+// O Carrinho é independente. Ele apenas escuta o 'cart:add'
+window.addEventListener('cart:add', (e) => {
+    const { nome, preco } = e.detail;
+    console.log(`[CARRINHO] Recebi notificação de adição: ${nome}`);
+    
+    cart.items.push({ nome, preco });
+    cart.total += preco;
+    updateCartUI();
+});
